2d21c31d84699ea2da86281b07ef23fc5ce9872a
Eman Rashad <iamybatish@gmail.com>
2024-11-15 12:00:47 +0200

    Delete imdb directory

--- imdb/src/pages/notPage.jsx
+++
@@ -1,8 +0,0 @@
-import { t } from "i18next";
-
-export default function NotPage() {
-    return (
-      <div className="text-danger mt-5 text-center fs-1">{t('Items Does Not found')}</div>
-    )
-  }
-  
